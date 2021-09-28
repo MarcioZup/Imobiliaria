eac5ae31af2b511639105c7ed4ad1f4c53793fbf
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-09-28 10:15:19 -0300

    Modelagem da classe Funcionario

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,2 +1,31 @@
 public class Funcionario extends Pessoa{
+
+    private double numeroDoCreci;
+
+    public Funcionario(double numeroDoCreci) {
+        this.numeroDoCreci = numeroDoCreci;
+    }
+
+    public Funcionario(String nome, String cpf, double salario, double numeroDoCreci) {
+        super(nome, cpf, salario);
+        this.numeroDoCreci = numeroDoCreci;
+    }
+
+    public double getNumeroDoCreci() {
+        return numeroDoCreci;
+    }
+
+    public void setNumeroDoCreci(double numeroDoCreci) {
+        this.numeroDoCreci = numeroDoCreci;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder retorno = new StringBuilder();
+        retorno.append("Nome: " + getNome());
+        retorno.append(" CPF: " + getCpf());
+        retorno.append(" Salário R$ " + getSalario());
+        retorno.append(" Número do Creci: " + numeroDoCreci);
+        return retorno.toString();
+    }
 }

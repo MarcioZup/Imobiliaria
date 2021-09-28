292bc9ae677fbb8afb42bbef362f5c7ad066e215
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-09-28 10:21:28 -0300

    Modelagem da classe Morador

--- src/Morador.java
+++ src/Morador.java
@@ -1,2 +1,31 @@
 public class Morador extends Pessoa{
+
+    private int tempoDeContrato;
+
+    public Morador(int tempoDeContrato) {
+        this.tempoDeContrato = tempoDeContrato;
+    }
+
+    public Morador(String nome, String cpf, double salario, int tempoDeContrato) {
+        super(nome, cpf, salario);
+        this.tempoDeContrato = tempoDeContrato;
+    }
+
+    public int getTempoDeContrato() {
+        return tempoDeContrato;
+    }
+
+    public void setTempoDeContrato(int tempoDeContrato) {
+        this.tempoDeContrato = tempoDeContrato;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder retorno = new StringBuilder();
+        retorno.append("Nome: " + getNome());
+        retorno.append(" CPF: " + getCpf());
+        retorno.append(" Salário R$ " + getSalario());
+        retorno.append(" Tempo de contrato: " + tempoDeContrato + " meses.");
+        return retorno.toString();
+    }
 }
